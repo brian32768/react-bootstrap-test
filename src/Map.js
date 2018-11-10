ae7745f73698503a0fab5bb73114c9881c016142
Brian Wilson <bwilson@co.clatsop.or.us>
2018-11-09 17:05:09 -0800

    Added scale bar.

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,27 @@
+// Map.js
+//
 import React from 'react';
 
 // I can compose a map here with
 // all its various controls such as zoom buttons and scalebars
 // and wrap it inside the Map component.
 
-import ScaleBar from './scalebar';
+class ScaleBar extends React.Component {
+    render() {
 
-export default class Map extends React.Component {
+        // There is a 'scalebar' class defined in App.css
+        // that controls the style for a scale bar.
+        return (
+            <scalebar>
+              <p>
+                {this.props.children}
+              </p>
+            </scalebar>
+        );
+    }
+}
+
+class Map extends React.Component {
     render() {
 
         // There is a 'map' class defined in App.css
@@ -17,8 +32,15 @@ export default class Map extends React.Component {
               <p>
                 {this.props.children}
               </p>
-              <ScaleBar>Scale: 1000 miles</ScaleBar>
+              <ScaleBar>Scale: 100 miles</ScaleBar>
             </map>
         );
     }
 }
+
+// Put the ScaleBar into the Map namespace.
+Map.ScaleBar = ScaleBar;
+
+export default Map;
+
+// That's all!
