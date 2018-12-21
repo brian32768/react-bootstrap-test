10329c9061de2576d58988547e386682076cbae9
Brian H Wilson <bwilson@co.clatsop.or.us>
2018-12-20 17:02:31 -0800

    still trying to figure out contexts

---
+++ src/control.js
@@ -0,0 +1,36 @@
+// control.js react-bootstrap-test
+//
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
+import {MapContext} from './map-context'
+import {ThemeContext} from './theme-context'
+
+/*
+    This control has to access properties both from its immediate parent ("source")
+    and from a distant ancestor ("map")
+    And maybe the ThemeContext too, just to make it more interesting.
+*/
+
+export default class Control extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="control">
+            {this.props.title} opacity {this.props.value}%
+            <Slider onChange={this.props.onChange} value={this.props.value}/>
+            </div>
+        );
+    }
+}
+Control.contextType = MapContext;
+
+Control.propTypes = {
+    source: PropTypes.string,
+    title: PropTypes.string,
+    onChange: PropTypes.func
+};
