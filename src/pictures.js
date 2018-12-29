d17e2615b765efe553c1d7c42bae16e26c9bb0f6
Brian H Wilson <bwilson@co.clatsop.or.us>
2018-12-28 16:57:02 -0800

    Experimenting with react-select So FANCY.. need something more comprehensible

--- src/pictures.js
+++ src/pictures.js
@@ -1,6 +1,6 @@
 // picture.js react-bootstrap-test
 //
-import React from "react"
+import React, { Component } from 'react'
 import {
     Carousel,
     CarouselItem,
@@ -8,25 +8,63 @@ import {
     CarouselIndicators,
     CarouselCaption
 } from 'reactstrap'
+import Select, { components } from 'react-select'
+//import 'react-select/dist/default.css'
 
 const items = [
     {
         src: require('/assets/Proud.jpg'),
-        header: 'Pigeon', text: ""
+        header: 'Pigeon',
+        text: ""
     },
     {
         src: require('/assets/walking_pigeons.gif'),
-        header: 'Pigeons, walking', text: ""
+        header: 'Pigeons, walking',
+        text: ""
     },
     {
         src: require('/assets/beaver.jpg'),
-        header: 'Beaver', text: ""
+        header: 'Beaver',
+        text: ""
     },
     {
         src: require('/assets/lewis_and_clark.jpg'),
-        header: 'Lewis and Clark', text: ""
+        header: 'Lewis and Clark',
+        text: ""
     },
 ];
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+];
+
+/*
+const SingleValue = ({ children, ...props }) => (
+    <components.SingleValue {...props}>
+      {children}
+    </components.SingleValue>
+);
+
+type State = {};
+
+class CustomControl extends Component<*, State> {
+  state = {};
+  render() {
+    return (
+      <Select
+        defaultValue={items[0]}
+        isClearable
+        components={{ SingleValue }}
+        isSearchable
+        name="header"
+        options={items}
+      />
+    );
+  }
+}
+*/
 
 export default class Pictures extends React.Component {
     constructor(props) {
@@ -34,6 +72,7 @@ export default class Pictures extends React.Component {
         this.state = {
             activeIndex : 0,
             animating: false,
+            selectedOption: null,
         }
         this.next     = this.next.bind(this);
         this.previous = this.previous.bind(this);
@@ -44,19 +83,25 @@ export default class Pictures extends React.Component {
        if (this.animating) return;
        const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
        this.setState({ activeIndex: nextIndex });
-     }
+    }
 
-     previous() {
+    previous() {
        if (this.animating) return;
        const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
        this.setState({ activeIndex: nextIndex });
-     }
+    }
 
-     onClick() {
+    onClick() {
          console.log("clicked");
-     }
+    }
+
+    handleChange(selectedOption) {
+       this.setState({ selectedOption });
+       console.log(`Option selected:`, selectedOption);
+    }
 
     render() {
+        const { selectedOption } = this.state;
         const { activeIndex } = this.state;
         const slides = items.map((item) => {
           return (
@@ -70,19 +115,26 @@ export default class Pictures extends React.Component {
         });
 
         return (
-          <Carousel
-            activeIndex={activeIndex}
-            next={this.next}
-            previous={this.previous}
-          >
-            <CarouselIndicators
-                items={items}
-                activeIndex={activeIndex}
-                onClickHandler={this.onClick} />
-            {slides}
-            <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
-            <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
-          </Carousel>
+            <div>
+            <Select
+              value={selectedOption}
+              onChange={this.handleChange}
+              options={options}
+            />
+                <Carousel
+                    activeIndex={activeIndex}
+                    next={this.next}
+                    previous={this.previous}
+                >
+                    <CarouselIndicators
+                        items={items}
+                        activeIndex={activeIndex}
+                        onClickHandler={this.onClick} />
+                    {slides}
+                    <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+                    <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+                </Carousel>
+            </div>
         );
     }
 }
