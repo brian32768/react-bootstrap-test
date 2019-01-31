b04dd7ad71372a06d7bcbe5aefb90dbb06f20f68
Brian H Wilson <brian@wildsong.biz>
2019-01-30 21:59:04 -0800

    Mo code

--- src/TasksPage.js
+++ src/TasksPage.js
@@ -6,14 +6,34 @@ import './App.css'
 
 export default class TasksPage extends Component {
     state = {
+        showForm: false,
         title: '',
         description: ''
     }
-    change = (e) => {
-        console.log("change", e.target.value);
+    resetForm() {
+        this.setState({
+            showForm: false,
+            title: '',
+            description: ''
+        });
     }
     submit = (e) => {
         console.log("submit", e.target.value);
+        e.preventDefault();
+        this.props.onCreateTask({
+            title: this.state.title,
+            description: this.state.description
+        });
+        this.resetForm();
+    }
+    //toggleForm(e) = (e) => {
+    //}
+    change = (e) => {
+        const { target: {name, value} } = e;
+        console.log("change", name, value);
+        this.setState({
+            [name]:value
+        });
     }
     render() {
         console.log("TasksPage", this.props.tasks);
