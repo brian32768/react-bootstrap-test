46e3562ef64580bacd72b45008e50bbc4de2f95e
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-03-12 16:55:31 -0700

    working on history using usng as a hash

---
+++ src/redux/middleware/README.md
@@ -0,0 +1,2 @@
+I based this example on "Learning Redux" from Safari.
+I think it's Chapter 10, "Extending the Redux Store via Middleware".
---
+++ src/redux/middleware/logger.js
@@ -0,0 +1,8 @@
+const logger = store => next => action => {
+    if (console.group) console.group(action.type)
+    console.info('dispatching', action)
+    const result = next(action)
+    console.log('new state', store.getState())
+    if (console.groupEnd) console.groupEnd(action.type)
+}
+export default logger
