a47b4c63f6d8a1fcf41ff5d5ad3d398c7a0914a5
Brian H Wilson <brian@wildsong.biz>
2019-06-10 22:11:23 -0700

    connected routing

--- src/redux/middleware/mapextent.js
+++ src/redux/middleware/mapextent.js
@@ -1,10 +1,20 @@
+import { push, replace } from 'connected-react-router'
+
 const DEFAULT_LOCATION_CHANGE = '@@router/LOCATION_CHANGE';
 
 const mapExtentMiddleware = store => {
     console.log('mapExtent middleware active');
     return next => action => {
         try {
-            console.log('mapExtentMiddleware action', action);
+            switch (action.type) {
+                case DEFAULT_LOCATION_CHANGE:
+                    console.log('mapExtentMiddleware action', action);
+                    // perhaps I can do a replace here?
+                    break;
+                default:
+                    console.log('uh mapExtentMiddleware action', action);
+                    break
+            }
             return next(action)
         } catch(err) {
             console.error('MapExtent Middleware Error', err)
