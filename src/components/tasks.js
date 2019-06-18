c4e04aa544f7e618429a54ac4db73730fb384e1b
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-06-18 15:18:22 -0700

    updated to work with redux-first-router

---
+++ src/components/tasks.js
@@ -0,0 +1,18 @@
+import React from 'react'
+//import NewTask from './newtask'
+//import TaskList from './tasklist'
+
+//<NewTask onCreateTask={ this.props.onCreateTask }/>
+//<TaskList tasks={ this.props.tasks }/>
+/*
+<NewTask />
+<TaskList />
+*/
+export const Tasks = () => {
+    console.log("Tasks are so fun!");
+    return (
+    <>
+    Here there be tasks
+    </>
+    );
+}
---
+++ src/reducers/page.js
@@ -0,0 +1,19 @@
+import { NOT_FOUND } from 'redux-first-router'
+
+const components = {
+    HOME:     'Home',
+    MAP:      'MapPage',
+    TABLE:    'Table',
+    PICTURES: 'Pictures',
+    ABOUT:    'About',
+    CONTACT:  'Contact',
+    TASKS:    'Tasks',
+
+    [NOT_FOUND]: 'NotFound'
+}
+
+export const page = (state = 'Home', action = {}) => {
+    const newState = components[action.type] || state
+    console.log("page reducer: ", action.type, " state=", state, " newState=", newState);
+    return newState
+}
