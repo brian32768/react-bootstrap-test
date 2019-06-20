94996b65c9e36d74351096416b9e0d85b5581882
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-06-20 15:59:14 -0700

    added thunk test temporarily so I can work at home

---
+++ thunk-test-app/src/configureStore.js
@@ -0,0 +1,18 @@
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import { connectRoutes } from 'redux-first-router'
+
+import routesMap from './routesMap'
+import page from './pageReducer'
+import swapi from './swapiReducer'
+
+export default function configureStore(preloadedState) {
+  const { reducer, middleware, enhancer } = connectRoutes(routesMap)
+
+  const rootReducer = combineReducers({ page, swapi, location: reducer })
+  const middlewares = applyMiddleware(middleware)
+  const enhancers = compose(enhancer, middlewares)
+
+  const store = createStore(rootReducer, preloadedState, enhancers)
+
+  return { store }
+}
