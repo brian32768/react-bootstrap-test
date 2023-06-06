fb9c6f5a17f754448f4fad5602a0cbcdf4c1d459
Brian H Wilson <bwilson@clatsopcounty.gov>
2023-06-06 12:09:55 -0700

    got debugger working

---
+++ react-simply/src/components/UncontrolledForm.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Button } from 'react-bootstrap'
+
+const MyForm = (props: {firstname: string, setFirstname: CallableFunction}) => {
+    const onSubmit = (e) => {
+        console.log('Submit');
+        props.setFirstname(window.firstname.value);
+        e.preventDefault();
+    }
+    return (
+        <>
+            <h1>Uncontrolled Form</h1>
+            This is an example of an uncontrolled form.
+            They are managed by the DOM not by the React code.
+            
+            <form onSubmit={onSubmit}>
+                <label>
+                    Name 
+                    <input id="firstname" required/>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+        </>
+    ); 
+}
+export default MyForm;
