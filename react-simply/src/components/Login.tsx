b257b0c354ef50d2d9d4d10ce3b3ff07a02bc577
Brian H Wilson <bwilson@clatsopcounty.gov>
2023-06-05 17:02:01 -0700

    chkpt

--- react-simply/src/components/Login.tsx
+++ react-simply/src/components/Login.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { Button } from 'react-bootstrap'
 
-const Login = ( props: object ) => {
+const Login = ( props: {username: string} ) => {
     return (
     <>
         {props.username}
     </>
     );
 }
+
 export default Login;
