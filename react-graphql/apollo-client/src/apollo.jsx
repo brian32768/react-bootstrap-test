27cceeeaf6dffa8ceb14388b3567c8ff89057c98
Brian H Wilson <bwilson@clatsopcounty.gov>
2023-06-09 16:46:50 -0700

    chkpt friday

--- react-graphql/apollo-client/src/apollo.jsx
+++ react-graphql/apollo-client/src/apollo.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client'
 
 const GET_INSTRUMENTS = gql`
-  query GetInstruments {
-    instruments {
-      id
-      firstname
-      lastname
+    query GetInstruments {
+        instruments {
+            id
+            firstname
+            lastname
+        }
     }
-  }
 `;
 
-function DisplayInstruments() {
+const PING = gql`
+    query Ping {
+        ping
+    }
+`;
+
+const DisplayInstruments = () => {
     const { loading, error, data } = useQuery(GET_INSTRUMENTS);
-  
+
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error.message}</p>;
   
@@ -23,13 +29,24 @@ function DisplayInstruments() {
         <br />
       </div>
     ));
-  }
+}
+
+const Ping = () => {
+    const { loading, error, data } = useQuery(PING);
+    if (loading) return <>Server starting up...</>;
+    if (error) return <>Connect failed: {error.message}</>;
+    const ping = "Freddo";
+    return (
+        <font color="lightgrey">{data.ping}</font>
+    );
+}
 
 const Apollo = () => {
     return (
         <div>
-        <h1>Apollo</h1>
-        <DisplayInstruments />
+            <h1>Apollo</h1>
+            <DisplayInstruments />
+            <Ping /><br />
         </div>
     )
 }
--- react-graphql/apollo-server/mock_data.js
+++ react-graphql/apollo-server/mock_data.js
@@ -1,14 +1,20 @@
 export const instruments = [
     {
-        "id": 123,
+        "id": 1,
         "firstname" : "James",
         "lastname" : "Dean",
         "recording_date": "01-01-1957"
     },
     {
-        "id": 456,
+        "id": 2,
         "firstname" : "Able",
         "lastname" : "Baker",
         "recording_date": "01-01-1967"
+    },
+    {
+        "id": 3,
+        "firstname" : "Lorna",
+        "lastname" : "Doon",
+        "recording_date": "01-01-1977"
     }
 ]
